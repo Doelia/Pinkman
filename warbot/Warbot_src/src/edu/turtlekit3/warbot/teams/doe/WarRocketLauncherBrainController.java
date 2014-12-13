3652f73ab396b8eeedabcd1e32e48e38837a8c4c
noe <noe.lephilippe@gmail.com>
2014-12-13 12:04:24 +0100

    ;

--- warbot/Warbot_src/src/edu/turtlekit3/warbot/teams/doe/WarRocketLauncherBrainController.java
+++ warbot/Warbot_src/src/edu/turtlekit3/warbot/teams/doe/WarRocketLauncherBrainController.java
@@ -18,13 +18,14 @@ import edu.turtlekit3.warbot.teams.doe.exceptions.NotExistException;
 
 public class WarRocketLauncherBrainController extends WarRocketLauncherAbstractBrainController {
 
-	int x = new Random().nextInt(1600);
-	int y = new Random().nextInt(1000);
+	int x;
+	int y;
 	String toReturn = "";
 	int maxDistanceToTarget = 50;
 
 	public WarRocketLauncherBrainController() {
 		super();
+		newPosition();
 	}
 	@Override
 	public String action() {
@@ -33,6 +34,11 @@ public class WarRocketLauncherBrainController extends WarRocketLauncherAbstractB
 		toReturn = move();
 		toReturn = attack();
 		return toReturn;
+	}
+	
+	public void newPosition() {
+		x = new Random().nextInt(3200) - 1600;
+		y = new Random().nextInt(2000) - 1000;
 	}
 
 	public String attack() {
@@ -54,25 +60,17 @@ public class WarRocketLauncherBrainController extends WarRocketLauncherAbstractB
 		try {
 			t = ev.getTeamManager().getTeamOf(this.getBrain().getID());
 			int leader = t.getLeader();
-			if(t.isBaseAttacked()) {
-				if(!getBrain().isReloaded()) {
-					Tools.setHeadingOn(
-							getBrain(), 
-							ev.getStructWarBrain(getBrain().getID()).getPosition(),
-							t.getDefensePosition(getBrain().getID()));
-					return WarRocketLauncher.ACTION_MOVE;
-				} else {
-					if(percept.size() > 0) {
-						getBrain().setHeading(percept.get(0).getAngle());
-						return WarRocketLauncher.ACTION_FIRE;
-					}
-				}
+			if(t.isBaseAttacked() && getBrain().getID() == leader) {
+				Tools.setHeadingOn(
+						getBrain(), 
+						ev.getStructWarBrain(getBrain().getID()).getPosition(),
+						new Vector2(0,0));
 			}
 			if(percept != null && percept.size() > 0){
 				t.setAttacking(true);
 				if(getBrain().getID() == leader) {
 					t.setTarget(ev.getEnemy(ev.getClosestEnemy(ev.getStructWarBrain(getBrain().getID()).getPosition())).getPosition(), 0);
-					//					t.setTarget(Tools.getPositionOfEntityFromMine(ev.getStructWarBrain(getBrain().getID()).getPosition(), percept.get(0).getAngle(), percept.get(0).getDistance()), (int) percept.get(0).getAngle());
+//					t.setTarget(Tools.getPositionOfEntityFromMine(ev.getStructWarBrain(getBrain().getID()).getPosition(), percept.get(0).getAngle(), percept.get(0).getDistance()), (int) percept.get(0).getAngle());
 				}
 			} else {
 				t.setAttacking(false);
@@ -114,14 +112,13 @@ public class WarRocketLauncherBrainController extends WarRocketLauncherAbstractB
 					}
 				} else {
 					int n = new Random().nextInt(100);
-					if(n > 95) {
-						x = new Random().nextInt(200);
-						y = new Random().nextInt(200);
+					if(n > 95 || getBrain().isBlocked()) {
+						newPosition();
 					}
 					Tools.setHeadingOn(
 							getBrain(), 
 							ev.getStructWarBrain(getBrain().getID()).getPosition(),
-							new Vector2(-x, -y));
+							new Vector2(x, y));
 					if(getBrain().isBlocked()) {
 						getBrain().setHeading((new Random().nextBoolean())?1:1 * 90 + getBrain().getHeading());
 					}
